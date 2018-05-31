d9a72b206e3dade93e7a8d3e48dec2e8f08b445a
hzh <define_hu@163.com>
2018-05-31 09:29:15 +0800

    add front

---
+++ front/src/components/GroupForm.vue
@@ -0,0 +1,49 @@
+<template>
+    <el-dialog title="dialog" :show-close="false" :visible="dialogFormVisible" >
+        <el-form :model="form">
+            <el-form-item label="temperature num" :label-width="formLabelWidth">
+                <el-input v-model="form.temperature" auto-complete="off"></el-input>
+            </el-form-item>
+            <el-form-item label="humidity num" :label-width="formLabelWidth">
+                <el-input v-model="form.humidity" auto-complete="off"></el-input>                
+            </el-form-item>
+            <el-form-item label="thunder num" :label-width="formLabelWidth">
+                <el-input v-model="form.thunder" auto-complete="off"></el-input>                
+            </el-form-item>
+            <el-form-item label="pressure num" :label-width="formLabelWidth">
+                <el-input v-model="form.pressure" auto-complete="off"></el-input>                
+            </el-form-item>
+        </el-form>
+        <div slot="footer" class="dialog-footer" :label-width="formLabelWidth">
+            <el-button @click="cancelForm">取 消</el-button>
+            <el-button type="primary" @click="upload(form)">确 定</el-button>
+        </div>
+    </el-dialog>
+</template>
+
+<script>
+export default {
+  name: "group-form",
+  data() {
+    return {
+      form : {
+        temperature: 0,
+        humidity: 0,
+        thunder: 0,
+        pressure: 0
+      },
+      formLabelWidth : "120px"
+    };
+  },
+  methods : {
+      cancelForm : function() {
+          this.$emit('cancelForm');
+      },
+      upload(form) {
+          this.$axios.post("http://127.0.0.1:8082/api/")
+      } 
+  },
+  props : ["dialogFormVisible"]
+};
+</script>
+
---
+++ front/src/components/ShowGroup.vue
@@ -0,0 +1,104 @@
+<template>
+  <div>
+    <div v-for="g in groupData" :key="g" class="box-card">
+      <button class="card-btn" @click="c(g.creater)">
+        <el-card>
+          <div slot="header" class="clearfix">
+            <div class="groupName">{{g.name}}</div>
+            <div class="groupDetail">
+              <span>{{g.creater}}</span><br>
+              <span>{{g.time}}</span>
+            </div>
+          </div>
+          <div v-for="o in g.client" :key="o.type" class="text item" style="{float: left;}">
+            {{o.type + ":" + o.num }}
+          </div>
+        </el-card>
+      </button>
+    </div>
+    <el-button type="text" @click="addNewGroup()">add new Group</el-button>
+    <group-form :dialogFormVisible="dialogFormVisible" v-on:cancelForm="dialogFormVisible=false"></group-form>
+
+  </div>
+</template>
+
+
+
+
+<script>
+import group from '../assets/data/group.json'
+import GroupForm from './GroupForm.vue'
+
+export default {
+  name: 'show-group',
+  data() {
+    return {
+      groupData: group,
+      formLabelWidth: '120px',
+      dialogFormVisible: false,
+    }
+  },
+  components: { GroupForm },
+  methods: {
+    c(object) {
+      this.$axios
+        .get('http://127.0.0.1:8082/api/test')
+        .then(function(response) {
+          var d = response.data
+          console.log(d)
+          for (var val in d) {
+            console.log(d[val])
+          }
+        })
+        .catch(function(res) {
+          console.log(res)
+        })
+    },
+    addNewGroup : function() {
+      this.dialogFormVisible = true;
+    }
+  }
+}
+</script>
+
+<style>
+.card-btn {
+  background-color: white;
+  border: 0ch;
+}
+.groupName {
+  float: left;
+  vertical-align: middle;
+  font-size: 2em;
+}
+.groupDetail {
+  float: left;
+  padding-left: 20px;
+  margin-left: 30px;
+  text-align: left;
+}
+
+.text {
+  font-size: 14px;
+}
+
+.item {
+  left: px;
+  margin-bottom: 18px;
+}
+
+.clearfix:before,
+.clearfix:after {
+  display: table;
+  content: '';
+}
+.clearfix:after {
+  clear: both;
+}
+
+.box-card {
+  float: left;
+  margin: 10px;
+  width: 250px;
+}
+</style>
